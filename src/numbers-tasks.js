97865d356552506a220b4760000a2f62c140458f
Avdasov Oleksandr <agentfreedom@zoho.com>
2023-11-08 23:40:24 +0200

    solution for task 12

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -221,8 +221,9 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  const num = parseInt(value, 10);
+  return Number.isNaN(num) ? def : num;
 }
 
 /**
