71afc8f59e5115c5fd4ffbabb2627599e54d84d5
Avdasov Oleksandr <agentfreedom@zoho.com>
2023-11-09 22:19:53 +0200

    solution for task 18

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -311,8 +311,11 @@ function getSumOfDigits(num) {
  *   16  => true
  *   15  => false
  */
-function isPowerOfTwo(/* num */) {
-  throw new Error('Not implemented');
+function isPowerOfTwo(num) {
+  for (let i = 1; i <= num; i *= 2) {
+    if (i === num) return true;
+  }
+  return false;
 }
 
 /**
